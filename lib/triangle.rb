55c8fd320c645e6654423e98a846f3794eeb9ea4
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 03:58:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,8 @@
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
-    # @side_1 = side_1
-    # @side_2 = side_2
-    # @side_3 = side_3
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
 
   end
 
