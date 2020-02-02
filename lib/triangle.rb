c1375c51b92407888d6721cba01c90f2b821ef31
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 04:26:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,27 +6,34 @@ class Triangle
 
   end
 
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] || sum_one_three > @triangle_sides[1] || sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
-  end
+  # def valid?
+  #   sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+  #   sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+  #   sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+  # 
+  #   if (@triangle_sides.none? {|side| side <= 0}) &&
+  #     (sum_one_two > @triangle_sides[2] || sum_one_three > @triangle_sides[1] || sum_two_three > @triangle_sides[0])
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
 
   def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
+      if ((@s1 * @s2 * @s3) == 0) || ((@s1 + @s2) <=@s3) || ((@s2 + @s3) <=@s1) || ((@s1 + @s3) <= @s2)
+        
+        begin
+         raise TriangleError
+           puts error.message
+         end
+
+      elsif @s1 == @s2 && @s2 == @s3
+        self.kind = :equilateral
+      elsif @s1 == @s2 || @s1 == @s3 || @s2 == @s3
+        self.kind = :isosceles
       else
-        return :scalene
+        self.kind = :scalene
+
       end
     else
       raise TriangleError
