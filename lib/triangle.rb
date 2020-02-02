861dc5ee7e06167b1a018a2dede315fee868e072
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 04:34:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,16 +23,16 @@ class Triangle
   # end
 
   def kind
-      if ((@s1 * @s2 * @s3) == 0) || ((@s1 + @s2) <=@s3) || ((@s2 + @s3) <=@s1) || ((@s1 + @s3) <= @s2)
+      if ((s1 * s2 * s3) == 0) || ((s1 + s2) <=s3) || ((s2 + s3) <=s1) || ((s1 + s3) <= s2)
 
         begin
          raise TriangleError
            puts error.message
          end
 
-      elsif @s1 == @s2 && @s2 == @s3
+      elsif s1 == s2 && s2 == s3
         self.kind = :equilateral
-      elsif @s1 == @s2 || @s1 == @s3 || @s2 == @s3
+      elsif s1 == s2 || s1 == s3 || s2 == s3
         self.kind = :isosceles
       else
         self.kind = :scalene
