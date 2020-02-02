ce09f3277d4179f8cbf99d8b918fa2e4e6e6d96f
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 04:36:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
   # end
 
   def kind
-      if ((s1 * s2 * s3) == 0) || ((s1 + s2) <=s3) || ((s2 + s3) <=s1) || ((s1 + s3) <= s2)
+      if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s1 + s3) <= s2
 
         begin
          raise TriangleError
